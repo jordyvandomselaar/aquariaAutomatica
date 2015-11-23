319fcb2643a54d61445e264a1768c5619faa0c5f
jordy .v. domselaar <jordy .v. domselaar>
2015-11-23 15:56:03 +0100

    Update

--- storage/views/index.php
+++ storage/views/index.php
@@ -5,22 +5,9 @@
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.css">
     <link rel="stylesheet" href="storage/styles/layout.css">
+    <link rel="import" href="bower_components/polymer/polymer.html">
   </head>
   <body>
-    <div class="container">
-      <h1>Welkom bij uw configuratie scherm</h1>
-      <div class="row">
-        <div class="col-md-4">
-          <p>Temperatuur: 20 graden</p>
-        </div>
-        <div class="col-md-4">
-          <p>Lichten: aan</p>
-        </div>
-        <div class="col-md-4">
-          <p>Filter: uit</p>
-        </div>
-      </div>
-    </div>
     <script src="bower_components/jquery/dist/jquery.js"></script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
   </body>
