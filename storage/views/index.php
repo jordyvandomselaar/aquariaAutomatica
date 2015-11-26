4a1f6ea8cc729b6c313a97acf8a876dcb0a007f6
jordy .v. domselaar <jordy .v. domselaar>
2015-11-26 10:30:43 +0100

    Remove bootstrap, fix font, add KAKU

--- storage/views/index.php
+++ storage/views/index.php
@@ -2,10 +2,9 @@
 <html>
   <head>
     <title>Aquaria Automatica</title>
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.css">
+    <script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+    <link rel="import" href="bower_components/font-roboto/roboto.html">
     <link rel="import" href="storage/styles/styles.html">
-    <link rel="import" href="bower_components/polymer/polymer.html">
     <link rel="import" href="bower_components/paper-card/paper-card.html">
     <link rel="import" href="bower_components/paper-tabs/paper-tabs.html">
     <link rel="import" href="bower_components/paper-toggle-button/paper-toggle-button.html">
@@ -13,43 +12,48 @@
     <link rel="import" href="bower_components/neon-animation/neon-animatable.html">
     <link rel="import" href="bower_components/neon-animation/neon-animations.html">
     <link rel="import" href="bower_components/iron-flex-layout/iron-flex-layout.html">
+    <link rel="import" href="bower_components/paper-slider/paper-slider.html">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
   </head>
-  <body>
-    <paper-tabs id="menu" selected="0">
-      <paper-tab>Arduino</paper-tab>
-      <paper-tab>KAKU</paper-tab>
-    </paper-tabs>
-    <div id="content">
-      <neon-animated-pages selected="0" entry-animation="slide-from-right-animation" exit-animation="slide-left-animation" id="pages">
-        <neon-animatable class="layout center-justified horizontal">
-          <div class="animatable_content">
-            <paper-card heading="Bedieningspaneel Aquarium">
-              <div class="card-content">
-                <div class="layout horizontal center-justified">
-                  <div class="layout vertical">
-                    <paper-toggle-button>Zuurstof</paper-toggle-button>
-                    <paper-toggle-button>Licht</paper-toggle-button>
+  <body unresolved class="fullbleed">
+    <div id="wrapper" unresolved>
+      <paper-tabs id="menu" selected="0">
+        <paper-tab>Arduino</paper-tab>
+        <paper-tab>KAKU</paper-tab>
+      </paper-tabs>
+      <div id="content">
+        <neon-animated-pages selected="0" entry-animation="slide-from-right-animation" exit-animation="slide-left-animation" id="pages">
+          <neon-animatable class="layout center-justified horizontal">
+            <div class="animatable_content">
+              <paper-card heading="Bedieningspaneel Aquarium">
+                <div class="card-content">
+                  <div class="layout horizontal center-justified">
+                    <div class="layout vertical">
+                      <paper-toggle-button>Zuurstof</paper-toggle-button>
+                      <paper-toggle-button>Licht</paper-toggle-button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </paper-card>
-          </div>
-        </neon-animatable>
-        <neon-animatable class="layout center-justified horizontal">
-          <div class="animatable_content">
-            <paper-card heading="Bedieningspaneel Aquarium">
-              <div class="card-content">
-                <div class="layout horizontal center-justified">
-                  <div class="layout vertical">
-                    <p>KAKU</p>
+              </paper-card>
+            </div>
+          </neon-animatable>
+          <neon-animatable class="layout center-justified horizontal">
+            <div class="animatable_content">
+              <paper-card heading="Bedieningspaneel Aquarium">
+                <div class="card-content">
+                  <div class="layout horizontal center-justified">
+                    <div class="layout vertical">
+                      <div>Temperatuur</div>
+                      <paper-slider id="temperature" pin min="0" max="50" secondary-progress="40" editable></paper-slider>
+                      <paper-toggle-button>Filter</paper-toggle-button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </paper-card>
-          </div>
-        </neon-animatable>
-      </neon-animated-pages>
+              </paper-card>
+            </div>
+          </neon-animatable>
+        </neon-animated-pages>
+      </div>
     </div>
     <script src="bower_components/jquery/dist/jquery.js"></script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
