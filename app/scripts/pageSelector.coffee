6433c78278d2a83dd5113b44ec0e620b6b95c493
jordy .v. domselaar <jordy .v. domselaar>
2015-12-01 11:59:43 +0100

    Update

--- app/scripts/pageSelector.coffee
+++ app/scripts/pageSelector.coffee
@@ -2,13 +2,12 @@ selected = 0
 
 $('#menu').click ->
     if selected < this.selected
-        setAnimation $('#pages'), 'slide-from-right-animation', 'slide-left-animation'
+    	setAnimation $('#pages'), 'slide-from-right-animation', 'slide-left-animation'
     else
         setAnimation $('#pages'), 'slide-from-left-animation', 'slide-right-animation'
     if this.selected != selected
         $('#pages')[0].select this.selected
     selected = this.selected
-
 
 
 setAnimation = (elem, enter, exit) ->
--- storage/views/index.php
+++ storage/views/index.php
@@ -16,44 +16,42 @@
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
   </head>
   <body unresolved class="fullbleed">
-    <div id="wrapper" unresolved>
-      <paper-tabs id="menu" selected="0">
-        <paper-tab>Arduino</paper-tab>
-        <paper-tab>KAKU</paper-tab>
-      </paper-tabs>
-      <div id="content">
-        <neon-animated-pages selected="0" entry-animation="slide-from-right-animation" exit-animation="slide-left-animation" id="pages">
-          <neon-animatable class="layout center-justified horizontal">
-            <div class="animatable_content">
-              <paper-card heading="Bedieningspaneel Aquarium">
-                <div class="card-content">
-                  <div class="layout horizontal center-justified">
-                    <div class="layout vertical">
-                      <paper-toggle-button>Zuurstof</paper-toggle-button>
-                      <paper-toggle-button>Licht</paper-toggle-button>
-                    </div>
+    <paper-tabs id="menu" selected="0">
+      <paper-tab>Arduino</paper-tab>
+      <paper-tab>KAKU</paper-tab>
+    </paper-tabs>
+    <div id="content">
+      <neon-animated-pages selected="0" entry-animation="slide-from-right-animation" exit-animation="slide-left-animation" id="pages">
+        <neon-animatable class="layout center-justified horizontal">
+          <div class="animatable_content">
+            <paper-card heading="Bedieningspaneel Aquarium">
+              <div class="card-content">
+                <div class="layout horizontal center-justified">
+                  <div class="layout vertical">
+                    <paper-toggle-button>Zuurstof</paper-toggle-button>
+                    <paper-toggle-button>Licht</paper-toggle-button>
                   </div>
                 </div>
-              </paper-card>
-            </div>
-          </neon-animatable>
-          <neon-animatable class="layout center-justified horizontal">
-            <div class="animatable_content">
-              <paper-card heading="Bedieningspaneel Aquarium">
-                <div class="card-content">
-                  <div class="layout horizontal center-justified">
-                    <div class="layout vertical">
-                      <div>Temperatuur</div>
-                      <paper-slider id="temperature" pin min="0" max="50" secondary-progress="40" editable></paper-slider>
-                      <paper-toggle-button>Filter</paper-toggle-button>
-                    </div>
+              </div>
+            </paper-card>
+          </div>
+        </neon-animatable>
+        <neon-animatable class="layout center-justified horizontal">
+          <div class="animatable_content">
+            <paper-card heading="Bedieningspaneel Aquarium">
+              <div class="card-content">
+                <div class="layout horizontal center-justified">
+                  <div class="layout vertical">
+                    <div>Temperatuur</div>
+                    <paper-slider id="temperature" pin min="0" max="30" secondary-progress="25" editable></paper-slider>
+                    <paper-toggle-button>Filter</paper-toggle-button>
                   </div>
                 </div>
-              </paper-card>
-            </div>
-          </neon-animatable>
-        </neon-animated-pages>
-      </div>
+              </div>
+            </paper-card>
+          </div>
+        </neon-animatable>
+      </neon-animated-pages>
     </div>
     <script src="bower_components/jquery/dist/jquery.js"></script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
